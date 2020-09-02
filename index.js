dccbaf2fa7d1caddfc38c23fd53d814f2977670f
tharindukw96 <tharindukw96@gmail.com>
2020-09-02 21:20:32 +0530

    Integrate with Heroku

--- index.js
+++ index.js
@@ -4,16 +4,10 @@
  */
 module.exports = app => {
   // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log('Yay, the app was loaded!');
 
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue! It worked.' })
     return context.github.issues.createComment(issueComment)
-  })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/ 
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  });
 }
